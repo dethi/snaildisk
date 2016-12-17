ba85edcba70539726d38bab2baa9c9a9b43656fc
Thibault Deutsch <thibault.deutsch@gmail.com>
2016-12-17 18:21:37 +0100

    Define global filters

---
+++ src/filters/index.js
@@ -0,0 +1,16 @@
+export default {
+  size(bytes) {
+    let size = bytes;
+
+    if (Math.abs(size) < 1024) {
+      return `${size} B`;
+    }
+    const units = ['kB', 'MB', 'GB', 'TB'];
+    let u = -1;
+    do {
+      size /= 1024;
+      u += 1;
+    } while (Math.abs(size) >= 1024 && u < units.length - 1);
+    return `${size.toFixed(1)} ${units[u]}`;
+  },
+};
