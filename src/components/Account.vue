9f1287b006129f5a5aa877446390d5dfa0f0bdf2
Thibault Deutsch <thibault.deutsch@gmail.com>
2016-12-17 18:24:00 +0100

    Account: use global filters

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <p>Hello {{ firstname }}!</p>
-    <p>Usage: {{ usedSpace | humanizeSize }} / {{ allocatedSpace | humanizeSize}} ({{ spaceUsage }}%)</p>
+    <p>Usage: {{ usedSpace | size }} / {{ allocatedSpace | size}} ({{ spaceUsage }}%)</p>
   </div>
 </template>
 
@@ -41,22 +41,6 @@ export default {
       return (this.allocatedSpace > 0)
         ? Math.round((this.usedSpace / this.allocatedSpace) * 100)
         : 0;
-    },
-  },
-  filters: {
-    humanizeSize(bytes) {
-      let size = bytes;
-
-      if (Math.abs(size) < 1024) {
-        return `${size} B`;
-      }
-      const units = ['kB', 'MB', 'GB', 'TB'];
-      let u = -1;
-      do {
-        size /= 1024;
-        u += 1;
-      } while (Math.abs(size) >= 1024 && u < units.length - 1);
-      return `${size.toFixed(1)} ${units[u]}`;
     },
   },
 };
