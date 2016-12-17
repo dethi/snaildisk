4dbcf79b1af62733ecb0281b2f0bcead2bac0315
Thibault Deutsch <thibault.deutsch@gmail.com>
2016-12-17 18:21:12 +0100

    Add Welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from './Login';
+
+export default {
+  components: {
+    Login,
+  },
+};
+</script>
