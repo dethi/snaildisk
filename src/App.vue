47c4cc1aa9168fe420ea5bfd20b034c3082427de
Thibault Deutsch <thibault.deutsch@gmail.com>
2016-12-17 18:23:40 +0100

    Register filters and router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <template>
   <div id="app">
-    <hello></hello>
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-import Hello from './components/Hello';
-
-export default {
-  name: 'app',
-  components: {
-    Hello,
-  },
-};
-</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  render: h => h(App),
+import App from './App';
+import router from './router';
+import filters from './filters';
+
+
+Object.keys(filters).forEach((key) => {
+  Vue.filter(key, filters[key]);
 });
+
+new Vue({
+  router,
+  ...App,
+}).$mount('#app');
