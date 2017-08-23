6025bf30cf709a4e651064b7b6b5ad4b0e28dcde
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-08-23 20:07:45 +0200

    chore: copy all files from root to the dist folder

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -93,6 +93,11 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../root'),
+        to: config.build.assetsRoot,
+        ignore: ['.*']
       }
     ]),
     // service worker caching
