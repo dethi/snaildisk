c19f70c111922d49d86b6177e9f617c12921c8bc
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-11-21 02:23:40 +0100

    view: set the login button url

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import Dropbox from 'dropbox';
+
 export default function LoginButton() {
+  const clientId = process.env.REACT_APP_DROPBOX_CLIENT_ID;
+  const loginUrl = process.env.REACT_APP_DROPBOX_REDIRECT_URL;
+
+  const dropbox = new Dropbox({ clientId });
+  const authUrl = dropbox.getAuthenticationUrl(loginUrl);
+
   return (
-    <a className="button is-outlined is-info is-large">
+    <a className="button is-outlined is-info is-large" href={authUrl}>
       <span className="icon is-medium">
         <i className="fa fa-dropbox" />
       </span>
