5b2713240342915047056b149692ecaa38ac2232
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-11-22 12:24:07 +0100

    chore: refactor action creator

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,5 @@
+import { makeActionCreator } from './helper';
+
 export const SET_TOKEN = 'auth:set_token';
 
-export function setToken(token) {
-  return { type: SET_TOKEN, token };
-}
+export const setToken = makeActionCreator(SET_TOKEN, 'token');
