bc358f575b59c50d57933dad93d7afcc75d56bd8
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-11-22 02:17:11 +0100

    api: fetch user account and space usage

--- src/api.js
+++ src/api.js
@@ -13,6 +13,24 @@ class API {
     const redirectUrl = process.env.REACT_APP_DROPBOX_REDIRECT_URL;
     return this.dropbox.getAuthenticationUrl(redirectUrl);
   }
+
+  async getAccount() {
+    const user = await this.dropbox.usersGetCurrentAccount();
+    return {
+      email: user.email,
+      firstname: user.name.given_name,
+      lastname: user.name.surname,
+      initials: user.name.abbreviated_name
+    };
+  }
+
+  async getSpaceUsage() {
+    const space = await this.dropbox.usersGetSpaceUsage();
+    return {
+      used: space.used,
+      allocated: space.allocation.allocated
+    };
+  }
 }
 
 const api = new API(process.env.REACT_APP_DROPBOX_CLIENT_ID);
