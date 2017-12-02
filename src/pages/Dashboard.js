5f5c3d4be8696f43544a7effc7564d536ad9e987
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-12-02 18:16:34 +0100

    view: display nav control

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import Accounts from '../components/Accounts';
+import AnalyseButton from '../components/AnalyseButton';
+import Stats from '../components/Stats';
 
 function Dashboard() {
   return (
     <section className="section">
       <div className="container">
         <Accounts />
+
+        <nav className="level">
+          <AnalyseButton />
+          <Stats />
+        </nav>
       </div>
     </section>
   );
